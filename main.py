8a286f690464fe3ef87aa7407ddc89d8b1c4405b
EC2 Default User <ec2-user@ip-172-31-17-251.eu-west-1.compute.internal>
2018-11-16 11:05:57 +0000

    Fixed main game loop. in alpha lol

--- main.py
+++ main.py
@@ -10,16 +10,41 @@ def get_question(dictionary):
    dict_size = len(dictionary)
    return dictionary[random.randint(0,dict_size-1)]
 
+def get_options(answer, dictionary):
+   dict_size = len(dictionary)
+   options = []
+   options.append(answer)
+   while(len(options) < 5):
+      opt = dictionary[random.randint(0,dict_size-1)]["Answer"]
+      if opt not in options:
+         options.append(opt)
+   return options
+
 if __name__ == '__main__':
    qdict = read_json()   
    while(True):
       item = get_question(qdict)
       question = item['Question']
-      answer = item['Answer'].lower()
-      something = input(answer).lower()
-      if(something == "exit"):
-         break
-      if(something == answer):
-         print("very good")
+      answer = item['Answer']
+      options = get_options(answer, qdict)
+      random.shuffle(options)
+      print("Select from one of these options")
+      count = 0
+      for o in options:
+         print(str(count)+". "+ o)
+         count += 1
+      count = 0
+      print("\n Here is your case")
+      something = input(question + " ").lower()
+      try:
+         result = options[int(something)]
+      except TypeError:
+         if(something == "exit"):
+            break
+         continue
+      if(result == answer):
+         print("\n very good \n")
+      else:
+         print("\n Wrong, the answer is " + answer + "\n")
 
   
